a0e1d73c887fb29ff979a6e24c2df2822f923ce4
tvpozytyw <tvpozytyw@gmail.com>
2020-07-30 23:05:05 +0200

    Change class name

--- src/main/java/com/mess/websocket/SessionDisconnectListener.java
+++ src/main/java/com/mess/websocket/SessionDisconnectListener.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
-public class Listener implements ApplicationListener<SessionDisconnectEvent> {
+public class SessionDisconnectListener implements ApplicationListener<SessionDisconnectEvent> {
     @Override
     public void onApplicationEvent(SessionDisconnectEvent sessionDisconnectEvent) {
         String name = sessionDisconnectEvent.getUser().getName();
